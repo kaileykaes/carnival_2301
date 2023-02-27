7d913407c145168c3ba774c0d0dff08fd227b67e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-27 09:54:10 -0700

    Test boards riders and logs

---
+++ spec/ride_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require './lib/ride'
+require './lib/visitor'
+
+RSpec.describe do 
+  before(:each) do 
+    @ride1 = Ride.new({ 
+      name: 'Carousel', 
+      min_height: 24, 
+      admission_fee: 1, 
+      excitement: :gentle 
+      })
+    @ride2 = Ride.new({ 
+      name: 'Ferris Wheel', 
+      min_height: 36, 
+      admission_fee: 5, 
+      excitement: :gentle 
+      })
+    @ride3 = Ride.new({ 
+      name: 'Roller Coaster', 
+      min_height: 54, 
+      admission_fee: 2, 
+      excitement: :thrilling 
+      })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+  end
+
+  it 'exists and has attributes' do 
+    expect(@ride1).to be_a(Ride)
+    expect(@ride1.min_height).to eq(24)
+  end
+
+  it 'has a rider log & total revenue' do 
+    expect(@ride1.rider_log).to eq({})
+    expect(@ride1.total_revenue).to eq(0)
+  end
+
+  it 'boards riders and logs them' do
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+  end
+end
