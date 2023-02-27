b82bba91e2c0f99a41e732c2411c97f4481a7b76
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-27 09:28:36 -0700

    create initialize and tall_enough? methods

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,17 @@
 class Visitor
-  
+  attr_reader :name, 
+              :height, 
+              :preferences,
+              :spending_money
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete_prefix('$').to_i
+    @preferences = []
+  end
+
+  def tall_enough?
+    @height >= 48
+  end
 end
