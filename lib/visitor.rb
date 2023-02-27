f5221d3d03d5f8959ccdfe8530d4bf01906b8812
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-27 10:40:37 -0700

    add wee method

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -2,13 +2,15 @@ class Visitor
   attr_reader :name, 
               :height, 
               :preferences,
-              :spending_money
+              :spending_money, 
+              :wee
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money.delete_prefix('$').to_i
     @preferences = []
+    @wee = false
   end
 
   def add_preference(preference)
