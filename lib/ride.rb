31eebade66e264c4abef522bd857671408a8d797
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-27 10:56:20 -0700

    define ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+  attr_reader :name, 
+              :min_height, 
+              :admission_fee, 
+              :excitement, 
+              :rider_log, 
+              :total_revenue
+
+  def initialize(details)
+    @name = details[:name]
+    @min_height = details[:min_height]
+    @admission_fee = details[:admission_fee]
+    @excitement = details[:excitement]
+    @rider_log = Hash.new(0)
+    @total_revenue = 0
+  end
  
+  def board_rider(visitor)
+    # visitor.spending_money - @admission_fee
+    # unless visitor.wee == true
+    #    @rider_log[visitor] += 1 
+       visitor.wee = true
+    end
+  end
 end
