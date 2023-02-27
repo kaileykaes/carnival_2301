9b5b1f67ec2939a67b58a070a5b5d3b413c9520a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-27 09:25:10 -0700

    Test: exists & has attr, and tall_enough? method

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/visitor'
+
+RSpec.describe do 
+  before(:each) do 
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  it 'exists and has attributes' do
+    expect(@visitor1).to be_a(Visitor)
+    expect(@visitor1.name).to eq('Bruce')
+    expect(@visitor1.height).to eq(54)
+    expect(@visitor1.spending_money).to eq(10)
+    expect(@visitor1.preferences).to eq([])
+  end
+
+  it 'determines acceptable height' do
+    expect(@visitor3.tall_enough?).to eq(false)
+    expect(@visitor1.tall_enough?).to eq(true)
+  end
+
+end
